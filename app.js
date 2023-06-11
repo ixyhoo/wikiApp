182d136153a0992c16719c77781053fbe4c1107a
ixyhoo <ixyhoo@outlook.fr>
2023-06-11 15:49:41 +0200

    add input  value

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const errorMsg = document.querySelector('.error-msg');
+
+form.addEventListener('submit',handleSubmit) 
+
+function handleSubmit(e){
+    e.preventDefault();
+    
+    if(!input.value.length){
+        errorMsg.textContent = 'Please enter a search term';
+        return;
+    }
+}

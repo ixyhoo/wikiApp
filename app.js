21fba07eadbe73055945dc51265ca2e2ce18c430
ixyhoo <ixyhoo@outlook.fr>
2023-06-11 17:05:23 +0200

    test ok

--- app.js
+++ app.js
@@ -12,5 +12,29 @@ function handleSubmit(e){
     if(input.value === ""){
         errorMsg.textContent = 'Please enter a search term';
         return;
+    } else {
+        errorMsg.textContent = "";
+        wikiApiCall(input.value);
     }
 }
+
+async function wikiApiCall(searchInput){
+ const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`)  
+
+const data = await response.json();
+ console.log(data);
+
+ creatCards(data.query.search);
+}
+
+function creatCards(data){
+    if (!data.length){
+        errorMsg.textContent = 'No results found';
+        return;
+    }
+    data.forEach(el => {
+        const card = document.createElement('div');
+        card.className = "result-item";
+        card.inertHTML
+    });
+}e
